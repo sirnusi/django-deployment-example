c322dd11182ffc629b2fdb5d33f7559d2d4ae632
sirnusi <38861084+sirnusi@users.noreply.github.com>
2020-06-23 11:07:50 +0100

    commit

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,3 +1,5 @@
-#from django.contrib import admin
-
+from django.contrib import admin
+from . import models
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Post)
--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    overview = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    comment_count = models.IntegerField(default=0)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
