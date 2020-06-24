01ccfdc40f22aeb19fba7d38728945ff5354eb20
sirnusi <38861084+sirnusi@users.noreply.github.com>
2020-06-24 16:14:54 +0100

    initial commit

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 # Create your models here.
-
-User = get_user_model
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -13,3 +11,6 @@ class Post(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     comment_count = models.IntegerField(default=0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
