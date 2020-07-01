3171dc29cb6c32dbbbd4757f6bcdef532b96bf52
sirnusi <38861084+sirnusi@users.noreply.github.com>
2020-07-01 20:12:33 +0100

    initial commit

--- blogapp/models.py
+++ blogapp/models.py
@@ -13,5 +13,6 @@ class Post(models.Model):
     comment_count = models.IntegerField(default=0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     thumbnail = models.ImageField()
+    
     def __str__(self):
         return self.title
